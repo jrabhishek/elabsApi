3634d71298105dc60ce9f10333608b3b43ea6871
Axle10 <saptarshi.sinha.ind@gmail.com>
2020-01-16 09:53:22 +0530

    db updated

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('kiitelabs_db', 'root', '', {
-	host: 'localhost',
-	dialect: 'mysql'
-});
+// const sequelize = new Sequelize('kiitelabs_db', 'root', '', {
+// 	host: 'localhost',
+// 	dialect: 'mysql'
+// });
 
+const sequelize = new Sequelize('d8jdid3lo4jlcd','jgxmpjrwvuwydy','a2d7125605031eca9320725ef4f1aa9d4be27ac1493cc0a89333208df045de3c',{
+	host: 'ec2-23-21-13-88.compute-1.amazonaws.com',
+	dialect: 'postgres'
+})
 module.exports = sequelize;
